c2eaaa49f97c5b4e8b4a7f1069cb68cb3ed433e6
ta4hands <aquattro@nd.edu>
2018-11-08 20:49:23 -0500

    Completed problem two of HW 9

--- Exercise09.R
+++ Exercise09.R
@@ -8,13 +8,38 @@ library('ggplot2')
 library('grid')
 library('gridExtra')
 
-# Load data
+# Load data into variable
 NBA <- read.table("NBAppgVwins.txt",header = TRUE)
 
+# Load data into ggplot
 f <- ggplot(data = NBA, aes(x = PPG, y = Wins))
+
+# Create scatterplot with trendline
 f + geom_point() + stat_smooth(method = "lm")
+
+
 
 # Problem 2
 
+# Load data into variable
 data <- read.table("data.txt",header = TRUE, sep = ",")
 
+# Load data into ggplot
+a <- ggplot(data = data, aes(x = region, y = observations))
+
+# Made a bar graph of the means of each region
+a + geom_bar(aes(x = as.factor(region), y = observations), 
+             stat = "summary", fun.y = "mean", fill = "black", 
+             color = "black") + theme_classic() + xlab("region") + 
+  ylab("observations")
+
+# Made a scatterplot of the data, x-axis seperated by region
+# and color coded by region
+a + geom_point(aes(color = region)) + theme_classic() + 
+  xlab("region") + ylab("observations") + geom_jitter()
+
+# The bar graph of the means seems to indicate that the data from each
+# region is very similar, since they all appear to have near equal
+# means. However, when looking at the scatterplot of the data, it is
+# clear that the regions vary significantly and only happen to have
+# similar means.
